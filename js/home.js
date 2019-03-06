5b7e31816d4da0de1475046e55993b254b9aa80a
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2019-03-06 12:46:26 -0600

    Cambiando envio de mensaje

--- js/home.js
+++ js/home.js
@@ -37,15 +37,9 @@ var app = {
 		//Esto es para web
 		var success = function () { alert('Message sent successfully'); };
         var error = function (e) { alert('Message Failed:' + e); };
-        var options = {
-            replaceLineBreaks: false, // true to replace \n by a new line, false by default
-            android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
-                //intent: '' // send SMS without opening any other app
-            }
-        };
+
         
-        sms.send("9515705278", "Hola mundo", options, success, error);
+        SMS.send("9515705278", "Hola mundo", success, error);
 	}
 };
 
